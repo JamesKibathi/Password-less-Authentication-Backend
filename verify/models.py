0d5db7f2ad919bad253767e439d4a1757f8d9a12
James Njenga <knjenga94@gmail.com>
2023-05-27 22:29:07 +0300

    Created a custom User model

--- verify/models.py
+++ verify/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
+
+
+
+# User class which inherits from AbstractBaseUser and PermissionsMixin.
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20)
+    last_otp = models.IntegerField(null=True)
+    otp_expiry = models.DateTimeField(null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    def __str__(self):
+        return self.email
